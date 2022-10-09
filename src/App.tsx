379c5af3552a8b701ce4242499e1f5f72135d0dc
Nate Bess <NathanBessler@outlook.com>
2022-10-09 17:35:56 -0500

    (9) - prettier changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import React, {useState, useEffect, forwardRef, useRef} from 'react';
-import './index.scss';
+import React, { useState, useEffect, forwardRef, useRef } from "react";
+import "./index.scss";
 
 //Components
-import Header from './1-Header/Header';
-import Pages from './2-Pages/Pages';
-import Feedback from './3-Feedback/Feedback';
-import Technical from './4-Technical/Technical';
-import Footer from './5-Footer/Footer';
+import Header from "./1-Header/Header";
+import Pages from "./2-Pages/Pages";
+import Feedback from "./3-Feedback/Feedback";
+import Technical from "./4-Technical/Technical";
+import Footer from "./5-Footer/Footer";
 
 function App() {
-  
   return (
     <div id="App">
       <Header />
       <Pages />
       <Feedback />
       <Technical />
-      <Footer />      
+      <Footer />
     </div>
   );
 }
